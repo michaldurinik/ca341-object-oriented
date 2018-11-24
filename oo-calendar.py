1f14a42467d96a08a68a7dad0ff1a09328f8ff89
Michal Durinik <michal.durnik2@mail.dcu.ie>
2018-11-24 17:22:07 +0000

    1st

---
+++ oo-calendar.py
@@ -0,0 +1,25 @@
+class Week:
+
+    def __init__(self):
+        self.days =
+
+
+
+class Calendar:
+
+    def __init__(self):
+        pass
+
+    def add_appoitment(day, start_time, duration):
+        pass
+
+    def remove_appoitment(day, start_time)
+        pass
+        #will see number of appoitments in day can remvoe by index
+
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
