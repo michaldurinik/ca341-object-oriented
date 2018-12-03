f4d1820ccfe6cc4e844751937b2b19dbffbffd3c
Michal Durinik <michal.durnik2@mail.dcu.ie>
2018-12-03 15:52:27 +0000

    changes

--- Calendar.py
+++ Calendar.py
@@ -4,16 +4,18 @@ import tkinter
 
 class Day:
 
-    def __init__(self, day="Monday", schedule=[]):
+    def __init__(self, day="default"):
         self.day = day
-        self.schedule = schedule
-        self.day_data = (self.day, self.schedule)
+        self.schedule = []
 
     def get_day(self):
         return self.day
 
     def get_schedule(self):
         return self.schedule
+
+    def __str__(self):
+        return "{:<10} {}" .format(self.day, " ".join([str(t) for t in self.get_schedule()]) + "\n")
 
 
 class Week(Day):
@@ -23,10 +25,17 @@ class Week(Day):
         self.week = [Day("Monday"), Day("Tuesday"), Day("Wednesday"), Day("Thursday"), Day("Friday"), Day("Saturday"),
                      Day("Sunday")]
 
+    def __iter__(self):
+        for d in self.week:
+            yield d
+
+    def __getitem__(self, idx):
+        return self.week[idx]
+
     def __str__(self):
         s = ""
-        for data in self.week:
-            s += (data.get_day() + " " + " ".join([str(t) for t in data.get_schedule()]) + "\n")
+        for d in self.week:
+            s += d.__str__()
         return s
 
 
@@ -36,9 +45,9 @@ class Calendar(Week, Day):
         super().__init__()
         pass
 
-    def add_appointment(self, d, start_time, duration):
+    def add_appointment(self, d, description="", start_time=0, finish_time=0):
         if d < 7:
-            self.week[d].schedule = [start_time, duration]
+            self.week[d].schedule.append([description, start_time, finish_time])
 
     def remove_appointment(d, start_time):
         pass
@@ -47,14 +56,26 @@ class Calendar(Week, Day):
     def __str__(self):
         return super().__str__()
 
+
+def parse_time(time):
+    h, m = 0, 0
+    if time.find(":"):
+        h, m = time.split(":")
+
 def main():
     #top = tkinter.Tk()
     #top.mainloop()
     c = Calendar()
-    print(c)
-    c.add_appointment(3, 9, 2)
+    c.add_appointment(4, 9, 2)
+    c.add_appointment(4, 12, 2)
+    c.add_appointment(4, "Tom", 15, 2)
+    c.add_appointment(4, 18, 2)
+    c.add_appointment(2, 13, 5)
+
+
     print(c)
 
+    print(c[1])
 
 if __name__ == "__main__":
     main()
